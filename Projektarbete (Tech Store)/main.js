63b9b2af38bdbea14a6213915c146b31420cee48
Sara Pellnor <sara.ahlquist@hotmail.com>
2022-10-31 15:23:27 +0100

    fixing in js

--- Projektarbete (Tech Store)/main.js
+++ Projektarbete (Tech Store)/main.js
@@ -1,8 +1,15 @@
-var listOfProducts;
-const productsList = document.querySelector(".productsContainer"); //gets product-div from index.html och saves it in a variable
-const cartNumb = document.querySelector(".cartValue") // gets div from index.html and stores it inside the variable "cartValue" 
+// variable where the products from json is stored
+var listOfProducts; 
+//gets product-div from index.html och saves it in a variable
+const productsList = document.querySelector(".productsContainer");
+// gets div from index.html and stores it inside the variable "cartNumb"
+const cartNumb = document.querySelector(".cartValue")  
+//Empty array where the products are stored in LS
+const cart = [];
+// A div container where we store our products from cart and make them visible.
+const mobileContainer = document.querySelector(".mobileContainer")
 
-/** Get products from the json file and store it in a gobal variable */
+// Get products from the json file and store it in a global variable 
 function loadProducts() {
     fetch("./products.json")
         .then(function (response) {
@@ -14,21 +21,18 @@ function loadProducts() {
         });
 }
 
-
+//If the key "item" is not in LS, this statement creates the key "item" with the value from "cart"
 function initSite() {
     loadProducts();
-    //If the key "item" is not in LS, this statement creates the key "item" with the value of "cart"
     if (!localStorage.getItem("item")){
         localStorage.setItem("item", JSON.stringify(cart));
     }
     
 }
 
-/** Uses the loaded products data to create a visible product list on the website */
+// Uses the loaded products data to create a visible product list on the website 
 function addProductsToWebpage() {
-    // Check your console to see that the products are stored in the listOfProducts varible.
-    listOfProducts.forEach((product, index) => { //loopen som går igenom varje product
-        //hämtar från "product" och lägger in produktobjekten i egna element i våran "productList"-div
+    listOfProducts.forEach((product, index) => {
         productsList.innerHTML += `
         <div class="product">
             <h1 class="titleProduct">${product.title}</h1>
@@ -37,44 +41,36 @@ function addProductsToWebpage() {
             <h3 class="priceProduct">${product.price} kr</h3>
             <button class="buttonProduct" onclick="addToCart(${index})"><a class="cartInBtn"><i class="fa-solid fa-cart-arrow-down"></i></a>Lägg till i kundvagnen</button>
         </div>
-
     `;
     });
 }
 
-function initCart() { // loads "onload" in shoppingCart.html to set cart number.
-    const num = JSON.parse(localStorage.getItem("item")) // gets key "itemsInCart" fom LS and stores it into a variable "num"
-    if (num.length > 0) { // if num length is bigger than 0
-        cartNumb.innerHTML = (num.length) // display "num" length (current items in cart in LS) in header
+// This function is initiated in shoppingcart.html, and it looks for the key "item" from LS.
+//If the number in LS is bigger than 0 we print that number, else we print an empty string.
+function initCart() {
+    const num = JSON.parse(localStorage.getItem("item")) 
+    if (num.length > 0) {
+        cartNumb.innerHTML = (num.length)
     } else {
         cartNumb.innerHTML = ""}
 }
 
-//Empty array where the products are stored in LS
-const cart = [];
-
-//Empty array where the number of products are stored in LS
-const cartValues = [];
-
-//Function add products to cart
+// This function adds the chosen product to the cart by looping through the list of products from JSON and compares it to the button index.
+// If they match, the item is pushed in to the variable newItem, and then returns the updated value to the  key "Item" in LS.
+// We then call on the function initCart() to update the number in header-cart.
 function addToCart(index) {
-
-    listOfProducts.forEach((product) => { //Loops through list of products
-        if (listOfProducts.indexOf(product) == index) { //If index of product match button index...
-            const newItem = JSON.parse(localStorage.getItem("item")); //Store "item" from LS in the variable "newItem" 
-            newItem.push(product); //Pushes product to "newItem"
-            localStorage.setItem("item", JSON.stringify(newItem)); //Sends back added value to the key "item" in LS
+    listOfProducts.forEach((product) => { 
+        if (listOfProducts.indexOf(product) == index) {
+            const newItem = JSON.parse(localStorage.getItem("item"));  
+            newItem.push(product); 
+            localStorage.setItem("item", JSON.stringify(newItem));
         }
     })
-    initCart(); //calls on function that sets the cart number on index.html
+    initCart();
 }
-/* console.log(cartValue) */
 
-// cart JS
-
-const mobileContainer = document.querySelector(".mobileContainer")
-
-function yeah() {
+// A function that creates elements for each object inside the JSON array and then sets an attribute to each variable.
+function createCartProduct() {
     mobileContainer.innerHTML = ""
     const getElementFromLS = JSON.parse(localStorage.getItem("item"));
         getElementFromLS.forEach((product, index) => {
@@ -96,14 +92,18 @@ function yeah() {
         trash.setAttribute("class", "trash")
         trashIcon.setAttribute("class", "fa-regular fa-trash-can")
 
+        // prints out information from the key "item" in LS 
         title.innerHTML = product.title
         price.innerHTML = product.price + " kr"
         btn.innerHTML = "Ta bort"
 
+        // attaches all elements inside the div "mobile"
         mobile.appendChild(img)
         mobile.appendChild(title)
         mobile.appendChild(price)
         mobile.appendChild(btn)
+        
+        // attaches div "mobile" inside the div "mobileContainer" from shoppingcart.html
         mobileContainer.appendChild(mobile)
         trash.appendChild(trashIcon)
         btn.appendChild(trash)
@@ -126,7 +126,7 @@ function cont(product) {
     const index = cart.findIndex(item => product.title == item.title)
     cart.splice(index, 1)
     localStorage.setItem("item", JSON.stringify(cart))
-    yeah()
+    createCartProduct()
     
 }  
 
@@ -141,6 +141,30 @@ function getTotalPrice() {
             totalPrice.innerHTML = "Totalt pris: " + sum + " kr";
        })
 }
+
+
+function finish() {
+    const cart = JSON.parse(localStorage.getItem("item"))
+    if (cart.length > 0) {
+        alert("Tack för ditt köp. Välkommen åter!")
+        clear()
+    } else {
+        alert("Din varukorg är tom. Lägg något i varukorgen!")
+    }
+}
+
+function clear() {
+    const buyBtn = document.querySelector(".buyBtn")
+    mobileContainer.innerHTML = ""
+    const totalPrice = document.querySelector(".totalPrice")
+    totalPrice.innerHTML = "Din varukorg är tom"
+    cartNumb.innerHTML = ""
+    localStorage.removeItem("item")
+    buyBtn.style.display = "none"
+    
+}
+
+
 // Add your code here, remember to brake your code in to smaller function blocks
 // to reduce complexity and increase readability. Each function should have
 // an explainetory comment like the one for this function, see row 22.
