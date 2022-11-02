f1f35b293ff9a0a74f6b2d580c1b6f7709f1006d
Sara Pellnor <sara.ahlquist@hotmail.com>
2022-11-02 14:09:45 +0100

    newUserFunction

--- Projektarbete (Tech Store)/module loginForms/form.js
+++ Projektarbete (Tech Store)/module loginForms/form.js
@@ -1,56 +1,67 @@
+
+// ----- gets divs from index.html och saves it in a variable
 const signUpDiv = document.querySelector(".signUp");
 const logInDiv = document.querySelector(".logIn");
 
+// ----- where all of the new users stored
 
+const users = [];
 
-    // ----- Elements and attributes to sign in form.
+// ----- stores the empty key "userCatalogue" in LS
 
-    const logInForm = document.createElement("form");
-    logInForm.setAttribute("class", "logInForm");
-    logInDiv.appendChild(logInForm);
+if (!localStorage.getItem("userCatalogue")) {
+    localStorage.setItem("userCatalogue", JSON.stringify(users))
+}
 
-    const usernameLabel = document.createElement("label");
-    usernameLabel.setAttribute("class", "usernameLabel");
-    usernameLabel.innerHTML = "Användarnamn:";
-    logInForm.appendChild(usernameLabel);
+// ----- DEFAULT - Elements and attributes to sign in form.
 
-    const usernameInput = document.createElement("input");
-    usernameInput.setAttribute("class", "usernameInput");
-    usernameInput.setAttribute("type", "text");
-    logInForm.appendChild(usernameInput);
+const logInForm = document.createElement("form");
+logInForm.setAttribute("class", "logInForm");
+logInDiv.appendChild(logInForm);
 
-    const passwordLabel = document.createElement("label");
-    passwordLabel.setAttribute("class", "passwordLabel");
-    passwordLabel.innerHTML = "Lösenord:";
-    logInForm.appendChild(passwordLabel);
+const usernameLabel = document.createElement("label");
+usernameLabel.setAttribute("class", "usernameLabel");
+usernameLabel.innerHTML = "Användarnamn:";
+logInForm.appendChild(usernameLabel);
 
-    const passWordInput = document.createElement("input");
-    passWordInput.setAttribute("class", "passWordInput");
-    passWordInput.setAttribute("type", "password");
-    logInForm.appendChild(passWordInput);
+const usernameInput = document.createElement("input");
+usernameInput.setAttribute("class", "usernameInput");
+usernameInput.setAttribute("type", "text");
+logInForm.appendChild(usernameInput);
 
-    const logInBtn = document.createElement("button");
-    logInBtn.setAttribute("class", "logInBtn");
-    logInBtn.innerHTML = "Logga in";
-    logInForm.appendChild(logInBtn);
+const passwordLabel = document.createElement("label");
+passwordLabel.setAttribute("class", "passwordLabel");
+passwordLabel.innerHTML = "Lösenord:";
+logInForm.appendChild(passwordLabel);
 
-    const newUserLabel = document.createElement("label");
-    newUserLabel.setAttribute("class", "newUserLabel");
-    newUserLabel.innerHTML = "Ny här?";
-    logInForm.appendChild(newUserLabel);
+const passWordInput = document.createElement("input");
+passWordInput.setAttribute("class", "passWordInput");
+passWordInput.setAttribute("type", "password");
+logInForm.appendChild(passWordInput);
 
-    const createNewUserBtn = document.createElement("button");
-    createNewUserBtn.setAttribute("class", "createNewUserBtn");
-    createNewUserBtn.setAttribute("type", "button");
-    createNewUserBtn.innerHTML = "Skapa ny användare";
-    logInForm.appendChild(createNewUserBtn);
-    createNewUserBtn.addEventListener("click", createNewUser);
+const logInBtn = document.createElement("button");
+logInBtn.setAttribute("class", "logInBtn");
+logInBtn.innerHTML = "Logga in";
+logInForm.appendChild(logInBtn);
 
+const newUserLabel = document.createElement("label");
+newUserLabel.setAttribute("class", "newUserLabel");
+newUserLabel.innerHTML = "Ny här?";
+logInForm.appendChild(newUserLabel);
 
+const createNewUserBtn = document.createElement("button");
+createNewUserBtn.setAttribute("class", "createNewUserBtn");
+createNewUserBtn.setAttribute("type", "button");
+createNewUserBtn.innerHTML = "Skapa ny användare";
+logInForm.appendChild(createNewUserBtn);
+createNewUserBtn.addEventListener("click", createNewUser);
+
+// ----- function that creates the signUpForm in innerhtml and makes it posible to create new user
 
 function createNewUser() {
 
     logInDiv.style.display = "none";
+    signUpDiv.style.display = "block";
 
     // ----- Elements and attributes to sign up form.
 
@@ -67,6 +78,7 @@ function createNewUser() {
     newUsernameInput.setAttribute("class", "newUsernameInput");
     newUsernameInput.setAttribute("type", "text");
     signUpForm.appendChild(newUsernameInput);
+    
 
     const newPasswordLabel = document.createElement("label");
     newPasswordLabel.setAttribute("class", "newPasswordLabel");
@@ -77,26 +89,59 @@ function createNewUser() {
     newPasswordInput.setAttribute("class", "newPasswordInput");
     newPasswordInput.setAttribute("type", "password");
     signUpForm.appendChild(newPasswordInput);
+    
 
     const newUserBtn = document.createElement("button");
     newUserBtn.setAttribute("class", "newUserBtn");
+    newUserBtn.setAttribute("type", "button");
     newUserBtn.innerHTML = "Skapa ny";
     signUpForm.appendChild(newUserBtn);
+    newUserBtn.addEventListener("click", newUser);
 
-    newUser();
-    return
+    const newUserBlanc = document.createElement("p");
+    newUserBlanc.setAttribute("class", "newUserBlanc");
+    signUpForm.appendChild(newUserBlanc);
+
+    const goBack = document.createElement("button")
+    goBack.setAttribute("class", "goBackBtn");
+    goBack.setAttribute("type", "button");
+    goBack.innerHTML = "Tillbaka";
+    signUpForm.appendChild(goBack);
+    goBack.style.display = "none";
+
+    // ----- function that stores the new user in the variable "users"-array
+
+    function newUser() {
+        const username = document.querySelector(".newUsernameInput").value;
+        const password = document.querySelector(".newPasswordInput").value;
+        const getUserCatalogue = JSON.parse(localStorage.getItem("userCatalogue"))
+
+        if (username.length && password.length > 0) {
+            getUserCatalogue.push ({username: username, password: password});
+            localStorage.setItem("userCatalogue", JSON.stringify (getUserCatalogue));
+            newUserBlanc.innerHTML = "Nu kan du logga in..."
+            username.innerHTML = ""
+            password.innerHTML = ""
+            newUsernameLabel.style.display = "none";
+            newUsernameInput.style.display = "none";
+            newPasswordLabel.style.display = "none";
+            newPasswordInput.style.display = "none";
+            newUserBtn.style.display = "none";
+            goBack.style.display = "block"
+            goBack.addEventListener("click", logInFunction);
+            } else {
+            newUserBlanc.innerHTML = "Du måste ange nytt namn och lösenord..."
+        }
+    }
 }
 
-if (!localStorage.getItem("userCatalogue")) {
-    localStorage.setItem("userCatalogue", JSON.stringify("users"))
-}
 
-let users = [];
 
-function newUser() {
+// ----- function that take us back to the logInForm
 
-const getUserCatalogue = JSON.parse(localStorage.getItem("userCatalogue"))
-let uNameInput = document.querySelector("newUsernameInput").value
-let uPassInput = document.querySelector("newPasswordInput").value
-if (uNameInput > 0)
+function logInFunction () {
+    logInDiv.style.display = "block";
+    signUpDiv.style.display = "none";
+    createNewUserBtn.style.display = "none";
+    newUserLabel.style.display = "none";
 }
